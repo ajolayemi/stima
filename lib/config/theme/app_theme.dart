a7c2b878ec4427dfcc3148df0a9f08e7abf545e4
Kehinde Jolayemi <ajolayemi82@gmail.com>
2026-06-02 12:29:48 +0200

    chore: updated theme

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -58,29 +58,38 @@ class AppColors {
 
 /// 🔠 Text Styles (Typography)
 class AppTextStyles {
+  static const fontFamily = 'Roboto';
+  static const regularFontFamily = 'RobotoRegular';
+  static const mediumFontFamily = 'RobotoMedium';
+  static const boldFontFamily = 'RobotoBold';
+
   static const h1 = TextStyle(
     fontSize: 24,
     fontWeight: FontWeight.w500,
     height: 1.5,
     color: AppColors.textPrimary,
+    fontFamily: mediumFontFamily,
   );
   static const h2 = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.w500,
     height: 1.5,
     color: AppColors.textPrimary,
+    fontFamily: mediumFontFamily,
   );
   static const h3 = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w500,
     height: 1.5,
     color: AppColors.textPrimary,
+    fontFamily: mediumFontFamily,
   );
   static const h4 = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w500,
     height: 1.5,
     color: AppColors.textPrimary,
+    fontFamily: mediumFontFamily,
   );
 
   static const body = TextStyle(
@@ -88,34 +97,40 @@ class AppTextStyles {
     fontWeight: FontWeight.w400,
     height: 1.5,
     color: AppColors.textPrimary,
+    fontFamily: regularFontFamily,
   );
   static const small = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.w400,
     height: 1.5,
     color: AppColors.textSecondary,
+    fontFamily: regularFontFamily,
   );
   static const tiny = TextStyle(
     fontSize: 12,
     fontWeight: FontWeight.w400,
     height: 1.5,
     color: AppColors.textTertiary,
+    fontFamily: regularFontFamily,
   );
 
   static const button = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w500,
     color: AppColors.white,
+    fontFamily: mediumFontFamily,
   );
   static const label = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w500,
     color: AppColors.textPrimary,
+    fontFamily: mediumFontFamily,
   );
   static const input = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w400,
     color: AppColors.textPrimary,
+    fontFamily: regularFontFamily,
   );
 }
 
@@ -150,18 +165,32 @@ class AppTheme {
     useMaterial3: true,
     fontFamily: "Roboto",
     scaffoldBackgroundColor: AppColors.backgroundPage,
-    colorScheme: const ColorScheme.light(
-      primary: AppColors.green600,
-      onPrimary: AppColors.white,
-      secondary: AppColors.blue600,
-      onSecondary: AppColors.white,
-      error: AppColors.red600,
-      // background: AppColors.backgroundPage,
-      surface: AppColors.white,
-      // onBackground: AppColors.textPrimary,
-      onSurface: AppColors.textPrimary,
-      onError: AppColors.white,
-    ),
+    // colorScheme: const ColorScheme.light(
+    //   primary: AppColors.green600,
+    //   onPrimary: AppColors.white,
+    //   secondary: AppColors.blue600,
+    //   onSecondary: AppColors.white,
+    //   error: AppColors.red600,
+    //   // background: AppColors.backgroundPage,
+    //   surface: AppColors.white,
+    //   // onBackground: AppColors.textPrimary,
+    //   onSurface: AppColors.textPrimary,
+    //   onError: AppColors.white,
+    // ),
+    // colorScheme: ColorScheme.fromSeed(
+    //   seedColor: AppColors.green600,
+    //   primary: AppColors.green600,
+    //   // onPrimary: AppColors.white,
+    //   // secondary: AppColors.blue600,
+    //   // onSecondary: AppColors.white,
+    //   // error: AppColors.red600,
+    //   // // background: AppColors.backgroundPage,
+    //   // surface: AppColors.white,
+    //   // // onBackground: AppColors.textPrimary,
+    //   // onSurface: AppColors.textPrimary,
+    //   // onError: AppColors.white,
+    // ),
+    colorScheme: ColorScheme.light(primary: AppColors.green600),
     textTheme: const TextTheme(
       displayLarge: AppTextStyles.h1,
       displayMedium: AppTextStyles.h2,
